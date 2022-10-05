cabe733db21dba01f8c55ae4e736996ee6e4ad7e
Watashi <ricqalex@gmail.com>
2022-10-05 13:22:27 +0200

    2
    2

--- ESIEAVERSEUnity/ESIEAVERSE/Assets/Scripts/CameraMovement.cs
+++ ESIEAVERSEUnity/ESIEAVERSE/Assets/Scripts/CameraMovement.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MouseLook : MonoBehaviour
+public class CameraMovement : MonoBehaviour
 {
 
     public float mouseSensitivity = 100f;
