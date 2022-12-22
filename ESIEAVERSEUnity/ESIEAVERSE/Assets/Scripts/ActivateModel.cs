1ded1fba7c1562129d465e8de584e01f0b1bc22d
Lucas <76529599+DrLulu94130@users.noreply.github.com>
2022-12-22 17:16:11 +0100

    Spawnpoint + bugfix choix du perso

--- ESIEAVERSEUnity/ESIEAVERSE/Assets/Scripts/ActivateModel.cs
+++ ESIEAVERSEUnity/ESIEAVERSE/Assets/Scripts/ActivateModel.cs
@@ -1,17 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class ActivateModel : MonoBehaviour
 {
+    PhotonView PV;
     [SerializeField] GameObject[] Player;
     // Start is called before the first frame update
+
     void Start()
     {
+        PV = GetComponent<PhotonView>();
         if (PlayerCustom.nj != -1)
         {
-            Player[PlayerCustom.nj].SetActive(true);
-            UnityEngine.Debug.Log("marche");
+            if (PV.IsMine)
+            {
+                Player[PlayerCustom.nj].SetActive(true);
+                UnityEngine.Debug.Log("marche");
+            }
         }
         else
         { 
