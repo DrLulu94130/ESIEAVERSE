9287bcbaed8991c53ad15d388b70293d8704b8f1
DenezFK <denez.fk@gmail.com>
2023-02-10 11:22:25 +0100

    Update code Emote Wheel

--- ESIEAVERSEUnity/ESIEAVERSE/Assets/Scripts/EmoteWheel.cs
+++ ESIEAVERSEUnity/ESIEAVERSE/Assets/Scripts/EmoteWheel.cs
@@ -35,4 +35,30 @@ public class EmoteWheel : MonoBehaviour
         EmoteWheelUI.SetActive(EmoteWheelIsOn);
     }
 
+
+    public void Top()
+    {
+        Debug.Log("Waving");
+    }
+    public void TopR()
+    {
+        Debug.Log("Salute");
+    }
+    public void TopL()
+    {
+        Debug.Log("Shake Hands");
+    }
+
+    public void Bottom()
+    {
+        Debug.Log("Searching Pockets");
+    }
+    public void BottomR()
+    {
+        Debug.Log("Laughing");
+    }
+    public void BottomL()
+    {
+        Debug.Log("Being Cocky");
+    }
 }
