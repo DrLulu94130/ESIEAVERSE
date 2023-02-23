96e91e72291e98259a61d6d4ac597b100530d0e3
Lucas <76529599+DrLulu94130@users.noreply.github.com>
2023-02-23 10:13:51 +0100

    je finis tt a lheure

--- ESIEAVERSEUnity/ESIEAVERSE/Assets/Scripts/Map/Tableau.cs
+++ ESIEAVERSEUnity/ESIEAVERSE/Assets/Scripts/Map/Tableau.cs
@@ -1,22 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
 using UnityEngine;
 
 public class Tableau : MonoBehaviour
 {
     public Camera Tab;
     public bool Trigger;
+    public Tableau Board;
+    [SerializeField] GameObject CameraHolder;
+    PhotonView PV;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Board = GetComponent<Tableau>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Tab.enabled = Trigger;
-
+        bool tab = Board.Trigger;
+        if (tab)
+        {
+            if (PV.IsMine)
+            {
+                Tab.enabled = true;
+                    CameraHolder.SetActive(false);
+            }
+        }
+        if(!tab)
+        {
+            if (PV.IsMine)
+            {
+                CameraHolder.SetActive(true);
+                Tab.enabled = false;
+            }
+        }
         if ( !GetComponent<Timer>().TimerRunOut )
         {
             GetComponent<Outline>().enabled = true;
