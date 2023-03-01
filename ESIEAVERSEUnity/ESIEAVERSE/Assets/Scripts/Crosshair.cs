fbfa5d6202a324fd44c002cb379fc420bb756038
DenezFK <denez.fk@gmail.com>
2023-03-01 18:36:45 +0100

    Crosshair fixed

---
+++ ESIEAVERSEUnity/ESIEAVERSE/Assets/Scripts/Crosshair.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crosshair : MonoBehaviour
+{
+    public static bool isOn = true;
+
+    public GameObject CrosshairUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CrosshairUI.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if( (Pause.isOn) || (EmoteWheel.isOn) || (Tableau.isOn))
+        {
+            CrosshairUI.SetActive(false);
+            isOn = false;
+        }
+        else
+        {
+            CrosshairUI.SetActive(true);
+            isOn = true;
+        }
+    }
+}
