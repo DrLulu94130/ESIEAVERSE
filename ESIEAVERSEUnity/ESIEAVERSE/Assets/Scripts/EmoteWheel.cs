508c244fec5dc14e7ebe2b2450c28ae38d461b8b
DenezFK <denez.fk@gmail.com>
2023-03-17 11:35:41 +0100

    EmoteWheel fonctionne (mais pas en ligne) Tahia Rafik

--- ESIEAVERSEUnity/ESIEAVERSE/Assets/Scripts/EmoteWheel.cs
+++ ESIEAVERSEUnity/ESIEAVERSE/Assets/Scripts/EmoteWheel.cs
@@ -56,52 +56,33 @@ public class EmoteWheel : MonoBehaviour
     public void Top()
     {
         Debug.Log("Waving");
-        anim.SetBool("Top", true);
-        Reset();
+        anim.SetTrigger("Top");
     }
     public void TopR()
     {
         Debug.Log("Salute");
-        anim.SetBool("TopR", true);
-        Reset();
+        anim.SetTrigger("TopR");
     }
     public void TopL()
     {
         Debug.Log("Shake Hands");
-        anim.SetBool("TopL", true);
-        Reset();
+        anim.SetTrigger("TopL");
     }
 
     public void Bottom()
     {
         Debug.Log("Searching Pockets");
-        anim.SetBool("Bottom", true);
-        Reset();
+        anim.SetTrigger("Bottom");
     }
     public void BottomR()
     {
         Debug.Log("Laughing");
-        anim.SetBool("BottomR", true);
-        Reset();
+        anim.SetTrigger("BottomR");
     }
     public void BottomL()
     {
         Debug.Log("Being Cocky");
-        anim.SetBool("BottomL", true);
-        Reset();
+        anim.SetTrigger("BottomL");
     }
 
-    public IEnumerator Reset()
-    {   
-        yield return new WaitForSeconds(3.2f);
-
-        anim.SetBool("Top", false);
-        anim.SetBool("TopR", false);
-        anim.SetBool("TopL", false);
-        anim.SetBool("Bottom", false);
-        anim.SetBool("BottomR", false);
-        anim.SetBool("BottomL", false);
-
-        anim.SetBool("Idling", true);
-    }
 }
